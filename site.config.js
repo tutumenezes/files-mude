66e5dfa01273b86641cd0f9a67fb2075d8255fc5
Ashinch <glaxyinfinite@outlook.com>
2021-06-13 02:16:03 +0800

    dark-mode

--- site.config.js
+++ site.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
   // basic site info (required)
   name: 'Ash\'s Knowledge Base',
-  domain: 'ashinch.com',
+  domain: 'www.ashinch.com',
   author: 'Ashinch',
 
   // open graph metadata (optional)
